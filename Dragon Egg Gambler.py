1195ddc0ab17879e25ae7fe37dfd388597da8f67
jmb-ops <kali@itfortress.localdomain>
2026-02-24 22:05:36 -0800

    added end game function and fixed some bugs

--- Dragon Egg Gambler.py
+++ Dragon Egg Gambler.py
@@ -1,44 +1,58 @@
 '''Dragon Egg Gambler
 '''
 import random
-
+#game items.
 gold = 0
 loot = []
+items = ['armor', 'pearls', 'diamonds',
+                       'Crown of a Fallen King',
+                       'Polished Coins', 'Glass Baubles',
+                       'Shiny Buttons', 'Beads & Trinkets',
+                       'Ember Eater Amulet (magic)',
+                       'Fire Quenched Stone (magic)',
+                       'Half Melted Knight Helmet', 'Silver Bracelets',
+                       'Gem Shards',]
 
-def tbd():
+def end_game():
+    '''this function is intended to ask the player
+    to play again or end the game loop.'''
+    global loot
+    player_choice = input('Play Again? y/n: ')
+    if player_choice == 'y':
+        print('Dragon nests are rare, dangerous, and '
+          'overflowing with treasure. \nYou’ve found '
+          'one... A massive egg resting atop a glittering '
+          'hoard. \nEvery tap risks waking what guards it.'
+          '\npress ? for more info.')
+        loot = []
+        main()
+    elif player_choice == 'n':
+        exit()
+
+def egg_tap():
     '''this function needs to provide the randomness for which
 the egg either gives gold, if so how much? or it gives a special
 item such as: armor, more gold, gems or other such treasures,
 or deals death.'''
     global gold
     global loot
-    #random number generated below to determine 1 of 3 possible
-    #actions to take.
-    tap_egg = random.randrange(100)
-    #print(tap_egg)
-    if 1 <= tap_egg <= 75:
-        #give gold
-        #print(tap_egg,'gold')
+    global items
+
+    tap_egg = random.choice(['gold', 'loot', 'lose'])
+    if tap_egg == 'gold':
+        #awards gold
         amt = random.randrange(1, 13)
         print('tap! tap!!')
         print(f'You found + {amt} gold!')
         gold += amt
-    elif 76 <= tap_egg <= 95:
-        #give item
-        #print(tap_egg, 'item')
-        item = random.choice(['armor', 'pearls', 'diamonds', 'Crown of a Fallen King',
-                       'Polished Coins', 'Glass Baubles',
-                       'Shiny Buttons', 'Beads & Trinkets',
-                       'Ember Eater Amulet (magic)',
-                       'Fire Quenched Stone (magic)',
-                       'Half Melted Knight Helmet', 'Silver Bracelets',
-                       'Gem Shards',])
+    elif tap_egg == 'loot':
+        #awards item
+        item = random.choice(items)
         print('tap! tap!!')
         print(f'You found + {item}!')
         loot.append(item)
-    elif 96 <= tap_egg <= 100:
-        #death option
-        #print(tap_egg, 'death')
+    elif tap_egg == 'lose':
+        #triggers game loss.
         print('tap! tap!!')
         print('A loud crack echoes as the egg breaks open.')
         if 'armor' in loot:
@@ -64,44 +78,43 @@ or deals death.'''
         else:
             print('The newborn’s fire engulfs you completely, '
             'melting you into nothing.')
-            exit()
+            end_game()
     
 def main():
-    #tbd()
-    #print for debugging
+    #this is the main game loop.
     print('[T]ap egg or [C]ash out')
     player = input('Player 1: ')
     while player != 'q':
         if player == 't':
-            #print('gold=', gold, 'loot=', loot)
-            tbd()
+            egg_tap()
         elif player == 'c':
             #write score to disk
-            #print(f'Player 1 score: gold = {gold} \
-            #loot = {loot}')
             with open('highscore.txt', 'w') as f:
                 save_name = input('enter name for '
                 'highscore: ')
                 print(f'{save_name} escapes with {gold} '
-                f'gold and {len(loot)} looted items: {loot}')
-                saved = save_name, gold, loot
+                    f'gold and {len(loot)} looted items: {loot}')
+                saved = (save_name, 'gold=',gold, ' loot=', len(loot),
+                loot)
                 f.write(str(saved))
-                exit()
+            end_game()
         elif player == 'i':
             print(f'Player 1 score: gold = {gold} '
-            f'loot = {loot}')
+            f'loot = {len(loot)}{loot}')
         elif player == 'highscore':
             with open('highscore.txt', 'r') as f:
                 data = f.read()
-                print(data)
+            print(data)
         elif player == '?':
             print('(press i for score & highscore'
                   ' to see the highscore, t to tap'
                 ' the egg, c to cash out, q to quit.')
         print('[T]ap egg or [C]ash out')
         player = input('Player 1: ')
+    exit()
     
 if __name__ == "__main__":
+    #game intro
     print('Dragon nests are rare, dangerous, and '
           'overflowing with treasure. \nYou’ve found '
           'one... A massive egg resting atop a glittering '
